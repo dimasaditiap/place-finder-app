6676b47eca612c8e3c822ea391c335a15790391e
manasyebukit <13516122@std.stei.itb.ac.id>
2018-12-28 12:46:16 +0700

    add custom input field

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,21 +1,47 @@
 import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, TextInput, StyleSheet } from 'react-native';
 
 import startMainTabs from '../MainTabs/startMainTabs';
+import DefaultInput from '../../components/UI/DefaultInput/DefaultInput';
+import HeadingText from '../../components/UI/HeadingText/HeadingText';
 
 class AuthScreen extends Component {
     loginHandler = () => {
         startMainTabs();
-    }
+    };
 
-    render () {
+    render() {
         return (
-            <View>
-                <Text>Auth Screen</Text>
-                <Button title="Login" onPress={this.loginHandler}/>
+            <View style={styles.container}>
+                <HeadingText>Please Log In</HeadingText>
+                <Button title="Login Here" />
+                <View style={styles.inputContainer}>
+                    <DefaultInput placeholder="Email" style={styles.input} />
+                    <DefaultInput placeholder="Password" style={styles.input} />
+                    <DefaultInput
+                        placeholder="Confirm Password"
+                        style={styles.input}
+                    />
+                </View>
+                <Button title="Submit" onPress={this.loginHandler} />
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    inputContainer: {
+        width: '75%'
+    },
+    input: {
+        backgroundColor: '#eee',
+        borderColor: '#bbb'
+    }
+});
 
 export default AuthScreen;
