aa6af2f14cbff36783c784104b4126fd775b3a55
manasyebukit <13516122@std.stei.itb.ac.id>
2018-12-30 22:12:16 +0700

    Locate user and add marker in map

--- src/components/PickLocation/PickLocation.js
+++ src/components/PickLocation/PickLocation.js
@@ -9,26 +9,76 @@ class PickLocation extends Component {
         focusedLoc: {
             latitude: -6.89148,
             longitude: 107.6107,
-            latitudeDelta: 0.0122,
+            latitudeDelta: 0.00522,
             longitudeDelta:
                 (Dimensions.get('window').width /
                     Dimensions.get('window').height) *
-                0.0122
-        }
+                0.00522
+        },
+        chosedLoc: false
+    };
+
+    pickLocationHandler = event => {
+        const coordinates = event.nativeEvent.coordinate;
+        this.map.animateToRegion({
+            ...this.state.focusedLoc,
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude
+        });
+        this.setState(prevState => {
+            return {
+                focusedLoc: {
+                    ...prevState.focusedLoc,
+                    latitude: coordinates.latitude,
+                    longitude: coordinates.longitude
+                },
+                chosedLoc: true
+            };
+        });
+    };
+
+    getLocationHandler = () => {
+        const self = this;
+        navigator.geolocation.getCurrentPosition(
+            pos => {
+                const coordsEvent = {
+                    nativeEvent: {
+                        coordinate: {
+                            latitude: pos.coords.latitude,
+                            longitude: pos.coords.longitude
+                        }
+                    }
+                };
+                self.pickLocationHandler(coordsEvent);
+            },
+            err => {
+                alert(
+                    'Fetching the position failed, please pick manually on the map'
+                );
+            }
+        );
     };
 
     render() {
+        let marker = null;
+        if (this.state.chosedLoc) {
+            marker = <MapView.Marker coordinate={this.state.focusedLoc} />;
+        }
         return (
             <View style={styles.container}>
                 <MapView
                     initialRegion={this.state.focusedLoc}
                     style={styles.map}
-                />
+                    onPress={this.pickLocationHandler}
+                    ref={ref => (this.map = ref)}>
+                    {marker}
+                </MapView>
                 <View style={styles.button}>
                     <BackgroundedButton
                         color="#29aaf4"
                         bgColor="white"
-                        brColor="#29aaf4">
+                        brColor="#29aaf4"
+                        onPress={this.getLocationHandler}>
                         Locate Me
                     </BackgroundedButton>
                 </View>
