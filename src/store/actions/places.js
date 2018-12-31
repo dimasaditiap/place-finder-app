e7ea73ee24c5f74333d53974be445f3badf38ab5
manasyebukit <13516122@std.stei.itb.ac.id>
2018-12-31 13:14:28 +0700

    Store image to firebase storage

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -1,11 +1,31 @@
 import { ADD_PLACE, DELETE_PLACE } from './actionTypes';
 
 export const addPlace = (placeName, location, image) => {
-    return {
-        type: ADD_PLACE,
-        placeName,
-        location,
-        image
+    return dispatch => {
+        const placeData = { name: placeName, location };
+        fetch(
+            'https://us-central1-place-finder-d3f4b.cloudfunctions.net/storeImage',
+            {
+                method: 'POST',
+                body: JSON.stringify({
+                    image: image.base64
+                })
+            }
+        )
+            .catch(err => console.log(err))
+            .then(res => res.json())
+            .then(parsedRes => {
+                console.log(parsedRes);
+            });
+        // fetch('https://place-finder-d3f4b.firebaseio.com/places.json', {
+        //     method: 'POST',
+        //     body: JSON.stringify(placeData)
+        // })
+        //     .catch(err => console.log(err))
+        //     .then(res => res.json())
+        //     .then(parsedRes => {
+        //         console.log(parsedRes);
+        //     });
     };
 };
 
