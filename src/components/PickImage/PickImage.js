e0d9579b92a423f8d2521c2bcf10e8c4bdfbbcd6
manasyebukit <13516122@std.stei.itb.ac.id>
2019-01-09 22:10:15 +0700

    Shrink size of image

--- src/components/PickImage/PickImage.js
+++ src/components/PickImage/PickImage.js
@@ -10,16 +10,19 @@ class PickImage extends Component {
     };
 
     pickImageHandler = () => {
-        ImagePicker.showImagePicker({ title: 'Pick an Image' }, res => {
-            if (res.didCancel) {
-                console.log('User cancelled');
-            } else if (res.error) {
-                console.log('Error :', res.error);
-            } else {
-                this.setState({ pickedImage: { uri: res.uri } });
-                this.props.onImagePick({ uri: res.uri, base64: res.data });
+        ImagePicker.showImagePicker(
+            { title: 'Pick an Image', maxWidth: 800, maxHeight: 600 },
+            res => {
+                if (res.didCancel) {
+                    console.log('User cancelled');
+                } else if (res.error) {
+                    console.log('Error :', res.error);
+                } else {
+                    this.setState({ pickedImage: { uri: res.uri } });
+                    this.props.onImagePick({ uri: res.uri, base64: res.data });
+                }
             }
-        });
+        );
     };
 
     render() {
